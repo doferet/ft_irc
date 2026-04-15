60ad9b353dc515a3d82710edde405c3751494c94
Dollyne Feret <doferet@f2r2s8.clusters.42paris.fr>
2026-04-15 18:00:13 +0200

    refacto(quit): msg d'erreur envoye

--- new_irc/Command/Quit.cpp
+++ new_irc/Command/Quit.cpp
@@ -6,7 +6,7 @@
 /*   By: doferet <doferet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/12 20:34:46 by doferet           #+#    #+#             */
-/*   Updated: 2026/04/15 17:39:41 by doferet          ###   ########.fr       */
+/*   Updated: 2026/04/15 17:54:42 by doferet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,20 +32,24 @@ void Quit::execute(Client &client, std::string &input)
         
     //     return;
     // }
+
+    
     if (input.empty())
         input = "";
-    std::map<std::string, Channel *>::iterator it = _channels.begin();
-    while (it != _channels.end())
-    {
-        it->second->sendMsgChannelMember(client, ":" + client.getNickname() + "!" + client.getUsername() + "@localhost QUIT :" + input + "\r\n", _cli, 1);
-        it->second->removeClient(client.getId());
-        if (it->second->isEmpty() == true)
-        {
-            delete it->second;
-            _channels.erase(it);
-        }
-        else
-            it++;
-    }
+        
+    // std::map<std::string, Channel *>::iterator it = _channels.begin();
+    // while (it != _channels.end())
+    // {
+    //     it->second->sendMsgChannelMember(client, ":" + client.getNickname() + "!" + client.getUsername() + "@localhost QUIT :" + input + "\r\n", _cli, 1);
+    //     it->second->removeClient(client.getId());
+    //     if (it->second->isEmpty() == true)
+    //     {
+    //         delete it->second;
+    //         _channels.erase(it);
+    //     }
+    //     else
+    //         it++;
+    // }
+    client.addToOutput(":" + client.getNickname() + " QUIT :Client Quit\r\n");
     client.setDisconnected(true);
 }
